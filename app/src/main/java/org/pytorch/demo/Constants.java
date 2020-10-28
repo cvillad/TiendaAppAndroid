538d1b78ca9feb10f9417b14cbb20179b2a98621
Carlos Villa <cvillad@uninorte.edu.co>
2020-10-28 16:11:40 -0500

    First commit
    Real time object classification

---
+++ app/src/main/java/org/pytorch/demo/Constants.java
@@ -0,0 +1,28 @@
+package org.pytorch.demo;
+
+public class Constants {
+  public static final String TAG = "App Tienda";
+
+  public static String[] IMAGENET_CLASSES = new String[]{
+          "aguacate",
+          "arroz_basmati_castellanox1000g",
+          "arroz_blanco_carullax1000g",
+          "arroz_vitamor_dianax500g",
+          "chicharron_americano_jacksx15g",
+          "coca_cola_originalx250ml",
+          "coca_cola_originalx400ml",
+          "mango_tommy",
+          "maracuya",
+          "pan_de_queso_la_frontera",
+          "pan_mantequilla_la_fronterax55g",
+          "papas_limon_margarita_packeconomicox300g",
+          "papas_limon_margaritax105g",
+          "papas_limon_margaritax110g",
+          "papas_limon_margaritax39g",
+          "spagheti_barilla",
+          "yogurt_alpina_finesse_fresax150g",
+          "yogurt_alpina_finesse_fresax180g",
+          "yogurt_alpina_finesse_frutos_rojosx180g",
+          "yogurt_alpina_finesse_melocotonx180g"
+  };
+}
